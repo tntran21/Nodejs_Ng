7a572616c791f75625c353cb3690e2603e2b21ae
tntran21 <tranthithuyngan21@gmail.com>
2018-07-12 14:35:39 +0700

    coding convension cho speciescontroller

--- ng-node/server/api/controllers/SpeciesController.js
+++ ng-node/server/api/controllers/SpeciesController.js
@@ -6,53 +6,99 @@
  */
 
 module.exports = {
-    create_species: function(req, res){
+    // Tạo một loại 
+    create_species: function(req, res) {
         var species_name = req.param('species_name');
-        if(!species_name || species_name === ''){
+        
+        if (!species_name || species_name === '') {
             return res.json({
                 status: 'error',
-                message: 'Bạn chưa nhập tên loại'
-            })
+                message: 'Bạn chưa nhập tên loại',
+            });
         }
-        Species.create({species_name:species_name}).exec(function(err, created){
-            if(err){return console.log(err);}
-            if(created){
+        
+        Species.create({ species_name: species_name }).exec(function(err, created) {
+            if (err) { return console.log(err); }
+
+            if (created) {
                 return res.json({
                     status: 'success',
                     message: 'Tạo thể loại thành công',
-                })
+                });
             }
         });
     },
-    get_species: function(req, res){
-       Species.find().exec(function(err, find){
-           if(err){return console.log(err);}
-           if(find){ 
+
+    // Lấy danh sách loại (Sử dụng .find thay cho .findOne)
+    get_species: function(req, res) {
+       Species.find().exec(function(err, find) {
+           if (err) { return console.log(err); }
+
+           if (find) { 
                return res.json({
                    status: 'success',
-                   message: 'Lay danh sach thanh cong',
-                   species: find
+                   message: 'Lấy danh sách thể loại thành công',
+                   species: find,
                });
            }
        });
     },
-    del_species: function(req, res){
+
+    // Xóa thể loại
+    del_species: function(req, res) {
         var species_id = req.param('species_id');
-        if(!species_id || species_id === ''){
+
+        if (!species_id || species_id === 0 || species_id === '') {
             return res.json({
                 status: 'error',
-                message: 'Id khong hop le'
+                message: 'ID không hợp lệ',
             });
         }
+
        Species.destroy({species_id: species_id}).exec(function(err){
            if(err){ return console.log(err);}
            return res.json({
                status: 'success',
-               message: 'Xoa loai thanh cong'
+               message: 'Xóa thể loại thành công'
            });
-       })
+       });
+    },
 
-    }
-	
+    // Update thông tin thể loại.
+    update_species: function(req, res) {
+        var species_id  = param("species_id");
+            species_name = param("species_name");
+
+        if(!species_id || species_id === 0 ||  species_id === ''){
+            return  res.json({
+                status: "error",
+                message: "ID không hợp lệ",
+            });
+        }
+
+        Species.findOne({ species_id: species_id }).exec(function(err, find){
+            if(err){ return console.log(err); }
+
+            if(find){
+                Species.update({ species_id: species_id }, {species_name: species_name}).exec(function(err, updated){
+                    if(err) { return console.log(err); }
+
+                    if(updated) {
+                        return  res.json({
+                            status: "success",
+                            message: "Update thể loại thành công",
+                        });
+                    }
+                });
+            }
+
+            else {
+                return  res.json({
+                    status: "error",
+                    message: "Không tìm thấy thể loại với ID là: " + species_id,
+                });
+            }
+        });
+    },
 };
 
--- ng-node/server/api/controllers/UsersController.js
+++ ng-node/server/api/controllers/UsersController.js
@@ -14,8 +14,7 @@ var jwt = require('../services/jsonwebtoken');
 
 module.exports = {
     // Tạo một người dùng mới. Người dùng được tạo bằng Email
-    user_create: function (req, res) 
-    {
+    user_create: function (req, res) {
         // Nhận dữ liệu từ người dùng gửi lên
         var client_user_email = req.param('client_user_email'),
             client_user_password = req.param('client_user_password');
@@ -27,6 +26,7 @@ module.exports = {
                 message: 'Bạn chưa nhập email',
             });
         }
+
         // Kiểm tra mật khẩu
         if (!client_user_password || client_user_password === '') {
             return res.json({
@@ -40,8 +40,7 @@ module.exports = {
          * Tìm thấy: Thông báo lỗi đã tồn tại.
          * Không tìm thấy email, cho tạo user.
          */
-        Users.findOne({ user_email: client_user_email }).exec(function (err, find) 
-        {
+        Users.findOne({ user_email: client_user_email }).exec(function (err, find) {
             // Nếu trong quá trình tạo bị lỗi, in lỗi ra màn hình.
             if (err) { return console.log(err); }
 
@@ -54,8 +53,7 @@ module.exports = {
 
             } else {
                 // Email của user chưa tồn tại, tiến hành tạo mới.
-                Users.create({ user_email: client_user_email, user_password: client_user_password }).exec(function (err, created) 
-                {
+                Users.create({ user_email: client_user_email, user_password: client_user_password }).exec(function (err, created) {
                     if (err) { return console.log(err) }
 
                     if (created) {
@@ -71,8 +69,7 @@ module.exports = {
     },
 
     // Xem thông tin của người dùng.
-    user_profile: function (req, res) 
-    {
+    user_profile: function (req, res) {
         var user_id = req.headers.authID;
         if (!user_id || user_id === '' || user_id === 0) {
             return res.json({
@@ -81,8 +78,7 @@ module.exports = {
             });
         }
 
-        Users.findOne({ user_id: user_id }).exec(function (err, find) 
-        {
+        Users.findOne({ user_id: user_id }).exec(function (err, find) {
             if (err) { return console.log(err); }
 
             if (find) {
@@ -106,8 +102,7 @@ module.exports = {
      * Nếu người dùng có nhập email thì không cho thay đổi email
      * Người dùng không nhập email thì cho thay đổi email nếu muốn.
      */
-    user_update: function (req, res) 
-    {
+    user_update: function (req, res) {
         // nhận dữ liệu từ người dùng gửi lên.
         var client_user_id = req.param('client_user_id'),
             update_email = req.param('update_update_email'),
@@ -123,16 +118,13 @@ module.exports = {
         }
 
         // Người dùng có nhập Email. Tìm
-        if ((update_email && update_email != '')) 
-        {
-            Users.findOne({ user_email: update_email }).exec(function (err, find)
-            {
+        if ((update_email && update_email != '')) {
+            Users.findOne({ user_email: update_email }).exec(function (err, find) {
                 if (err) { return console.log(err); }
 
                 if (find) {
                     // Không cho update email
-                    Users.update({ user_id: client_user_id }, { user_fullname: update_user_fullname, user_sex: update_user_sex }).exec(function (err, updated) 
-                    {
+                    Users.update({ user_id: client_user_id }, { user_fullname: update_user_fullname, user_sex: update_user_sex }).exec(function (err, updated) {
                         if (err) { return console.log(err); }
 
                         if (updated) {
@@ -143,8 +135,9 @@ module.exports = {
                         }
                     });
 
-                // Không tìm thấy user_email tồn tại trong csdl, cho update email.
-                } else {
+                    // Không tìm thấy user_email tồn tại trong csdl, cho update email.
+                } 
+                else {
                     Users.update({ user_id: client_user_id }, { user_email: update_email, user_fullname: user_fullname, user_sex: user_sex }).exec(function (err, updated) {
                         if (err) { return console.log(err) }
 
@@ -175,7 +168,8 @@ module.exports = {
                         }
                     });
 
-                } else {
+                } 
+                else {
                     return res.json({
                         status: 'error',
                         message: 'Không tìm thấy User với id: ' + user_id,
@@ -208,7 +202,7 @@ module.exports = {
             else {
                 return res.json({
                     status: 'error',
-                    message: 'Không tìm thấy User với ID: ' + user_id
+                    message: 'Không tìm thấy User với ID: ' + user_id,
                 })
             }
         })
@@ -219,22 +213,24 @@ module.exports = {
      * Nhập Email, kiểm tra password.
      * Đăng nhập thành công mã hóa user tạo token.
      */
-    login: function (req, res) 
-    {
+    login: function (req, res) {
         var user_email = req.param('user_email'),
             user_password = req.param('user_password');
+
         if (!user_email || user_email === '') {
             return res.json({
                 status: 'error',
-                message: 'Bạn chưa nhập Email'
+                message: 'Bạn chưa nhập Email',
             });
         }
+
         if (!user_password || user_password === '') {
             return res.json({
                 status: 'error',
-                message: 'Bạn chưa nhập mật khẩu'
+                message: 'Bạn chưa nhập mật khẩu',
             });
         }
+
         Users.findOne({ user_email: user_email }).exec(function (err, find) {
             if (err) { return console.log(err) }
 
@@ -242,6 +238,7 @@ module.exports = {
                 // Tìm thấy tiến hành so sánh password đã được mã hóa
                 Users.comparePassword(user_password, find, function (err, valid) {
                     if (err) { return console.log(err) }
+
                     if (valid) {
                         return res.json({
                             status: 'success',
@@ -250,18 +247,20 @@ module.exports = {
                             token: jwt.encode(find.user_id),
                         });
 
-                    } else {
+                    } 
+                    else {
                         return res.json({
                             status: 'error',
                             message: 'Mật khẩu không đúng',
                         });
                     }
                 });
-            // Nhập Email không đúng, không tìm thấy email    
-            } else {
+                // Nhập Email không đúng, không tìm thấy email    
+            } 
+            else {
                 return res.json({
                     status: 'error',
-                    message: 'Email không đúng'
+                    message: 'Email không đúng',
                 })
             }
         });
@@ -298,8 +297,7 @@ module.exports = {
             });
         }
 
-        Users.findOne({ user_id: user_id }).exec(function (err, find) 
-        {
+        Users.findOne({ user_id: user_id }).exec(function (err, find) {
             if (err) { return console.log(err); }
 
             if (find) {
@@ -308,17 +306,14 @@ module.exports = {
                     if (err) { return console.log(err); }
                     if (vaild) {
                         // Đúng, mã hóa mật khẩu mới trước khi lưu vào csdl
-                        bcrypt.genSalt(10, function (err, salt) 
-                        {
+                        bcrypt.genSalt(10, function (err, salt) {
                             bcrypt.hash(new_password, salt, function (err, hash) {
                                 if (err) {
                                     return cb(err);
                                 }
 
-                                if (hash) 
-                                {
-                                    Users.update({ user_id: user_id }, { user_password: hash }).exec(function (err, updated) 
-                                    {
+                                if (hash) {
+                                    Users.update({ user_id: user_id }, { user_password: hash }).exec(function (err, updated) {
                                         if (err) { return cb(err);; }
 
                                         if (updated) {
@@ -332,8 +327,9 @@ module.exports = {
                             });
                         });
 
-                    // Nhập mật khẩu cũ không đúng.
-                    } else {
+                        // Nhập mật khẩu cũ không đúng.
+                    } 
+                    else {
                         return res.json({
                             status: 'error',
                             message: 'Mật khẩu cũ không đúng',
@@ -347,7 +343,7 @@ module.exports = {
             else {
                 return res.json({
                     status: 'error',
-                    message: 'Không tìm thấy User với ID: ' + user_id
+                    message: 'Không tìm thấy User với ID: ' + user_id,
                 });
             }
         });
